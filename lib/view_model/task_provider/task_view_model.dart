794fceb4a2f2698a9a2c54e1bb5912abe8806f48
AbuRasel51 <rasel@primetechbd.com>
2025-12-12 17:39:42 +0600

    task view model start

--- lib/view_model/task_provider/task_view_model.dart
+++ lib/view_model/task_provider/task_view_model.dart
@@ -6,45 +6,6 @@ import 'package:location_based_task_management_app/repository/task_repo/task_rep
 import '../../model/task_model.dart';
 
 class TaskViewModel with ChangeNotifier{
-  final TaskRepository repository;
-  TaskViewModel({required this.repository});
-  //variable
-  Stream<List<TaskModel>>? _agentTaskList;
-  TaskViewModel? _taskById;
-  bool _isLoading=false;
-
-  //get list
-  Stream<List<TaskModel>>?get agentTaskList=>_agentTaskList;
-  TaskViewModel? get taskById=>_taskById;
-  bool get isLoading=>_isLoading;
-
-  //method
-  void setLoading({required bool loadingValue}){
-    _isLoading=loadingValue;
-    notifyListeners();
-  }
-
-  Future<void> todayTaskList({ required String agentId})async{
-    try{
-      _agentTaskList=repository.taskForAgentToday(agentId);
-      notifyListeners();
-    }catch(e){
-      log("error $e");
-      throw Exception(e);
-    }
-
-
-
-
-
-  }
-  Future<void>taskListById({required String id}) async {
-    _taskById=await repository.getTaskById(id);
-
-  }
-
-
-
 
 
 
